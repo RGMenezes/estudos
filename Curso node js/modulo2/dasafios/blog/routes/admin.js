41dd7666803610c395a267784e01466f05ea541f
Rafael Menezes <faelgmp@gmail.com>
2023-06-06 16:45:24 -0300

    criando o form de categoria

--- Curso node js/modulo2/dasafios/blog/routes/admin.js
+++ Curso node js/modulo2/dasafios/blog/routes/admin.js
@@ -5,8 +5,16 @@ router.get('/', (req, res) => {
     res.render("admin/index");
 });
 
-router.get('/posts', (req, res) => {
+router.get("/posts", (req, res) => {
     res.send("Página de posts");
+});
+
+router.get('/categorias', (req, res) => {
+    res.render("admin/categorias");
+});
+
+router.get('/categorias/add', (req, res) => {
+    res.render("admin/addcategorias");
 });
 
 router.get('/categorias', (req, res) => {
