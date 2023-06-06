b337ec766a792353c63ec4b59b56d8fce556abfb
Rafael Menezes <faelgmp@gmail.com>
2023-06-05 21:56:18 -0300

    trabalhando com mongoose

--- Curso node js/modulo2/mongo/aprendendo.js
+++ Curso node js/modulo2/mongo/aprendendo.js
@@ -10,3 +10,48 @@ const mongoose = require('mongoose');
     }).catch((error) => {
         console.log("Servidor não iniciado, erro: " + error);
     });
+
+
+//Criando models
+
+//definindo
+    const UsuarioSchema = mongoose.Schema({
+        nome: {
+            type: String,
+            require: true
+        },
+        sobrenome: {
+            type: String,
+            require: true
+        },
+        email: {
+            type: String,
+            require: true
+        },
+        idade: {
+            type: Number,
+            require: true
+        },
+        pais:  {
+            type: String
+        }
+    });
+
+//collection
+    mongoose.model('usuarios', UsuarioSchema);
+
+
+
+//Criando e adicionando dados na collection
+const Rafael = mongoose.model("usuarios");
+new Rafael({
+    nome: "Rafael",
+    sobrenome: "Menezes",
+    email: "email@email.com",
+    idade: 18,
+    pais: "Brasil"
+}).save().then(() => {
+    console.log("Usuario salvo com sucesso!");
+}).catch((error) => {
+    console.log("Não foi possivel salvar o usuario, ERRO: " + error)
+});
