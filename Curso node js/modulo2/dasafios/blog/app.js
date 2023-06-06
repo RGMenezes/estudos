9c1f642c38d2d6373890169aa0ceb10a750fd969
Rafael Menezes <faelgmp@gmail.com>
2023-06-05 23:32:28 -0300

    inicinando novo desafio (blog)

---
+++ Curso node js/modulo2/dasafios/blog/app.js
@@ -0,0 +1,46 @@
+//Carregando módulos
+
+const express = require('express');
+const app = express();
+const {engine} = require('express-handlebars');
+const bodyParser = require('body-parser');
+const admin = require('./routes/admin')
+// const mongoose = require('mongoose');
+
+//Configurações
+    //Body Parser
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.json());
+
+    //Handlebars
+    app.engine('handlebars', engine({
+        defaultLayout: 'main',
+        runtimeOptions:{
+            allowProtoPropertiesByDefault: true,
+            allowProtoMethodsByDefault: true
+        }
+    }));
+    app.set('view engine', 'handlebars');
+    app.set('views', './views');
+
+    //Mongoose
+        //Em breve
+
+    //
+
+//Rotas
+
+app.get('/', (req, res) => {
+    res.send("Rota principal");
+});
+
+app.get('/posts', (req, res) => {
+    res.send("Lista de posts");
+});
+
+app.use('/admin', admin);
+
+//Outros
+const PORT = 8081;
+
+app.listen(PORT, () => console.log("Servidor rodando...") );
---
+++ Curso node js/modulo2/dasafios/blog/routes/admin.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send("Página principal do painel ADM");
+});
+
+router.get('/posts', (req, res) => {
+    res.send("Página de posts");
+});
+
+router.get('/categorias', (req, res) => {
+    res.send("Página de categorias");
+});
+
+
+module.exports = router;
