d155f121d72081aaadc205f126cd0bc33b63ad39
Rafael Menezes <faelgmp@gmail.com>
2023-06-07 12:58:07 -0300

    Criando edicao de categoria

--- Curso node js/modulo2/dasafios/blog/routes/admin.js
+++ Curso node js/modulo2/dasafios/blog/routes/admin.js
@@ -54,7 +54,58 @@ router.post("/categorias/nova", (req, res) => {
             res.redirect("/admin/categorias");
         }).catch((err) => {
             req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente!");
-            res.redirect("/admin");
+            res.redirect("/admin/categorias");
+        });
+    };
+});
+
+router.get("/categorias/edit/:id", (req, res) => {
+    Categorias.findOne({_id: req.params.id}).then((categoria) => {
+        res.render("admin/editcategoria", {categoria: categoria});
+    }).catch((err) => {
+        req.flash("error_msg", "Esta categoria não existe");
+        res.redirect("/admin");
+    });
+});
+
+router.post('/categorias/edit', (req, res) => {
+    let erros = [];
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"});
+    };
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug inválido"});
+    };
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria é muito pequeno"});
+    };
+
+    if(erros.length > 0){
+        Categorias.findOne({_id: req.body.id}).lean().then((categoria) => {
+            res.render("admin/editcategoria", {categoria: categoria, erros: erros});
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao pegar dados!");
+                res.redirect("/admin/categorias");
+        });
+    }else{
+
+        Categorias.findOne({_id: req.body.id}).then((categoria) => {
+            categoria.nome = req.body.nome;
+            categoria.slug = req.body.slug;
+
+            categoria.save().then(() => {
+                req.flash('success_msg', 'Categoria editada com sucesso!');
+                res.redirect("/admin/categorias");
+            }).catch((err) => {
+                req.flash("error_msg", "Houve um erro interno ao salvar a edição da categoria!");
+                res.redirect("/admin/categorias");
+            });
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao editar a categoria!");
+            res.redirect("/admin/categorias");
         });
     };
 });
