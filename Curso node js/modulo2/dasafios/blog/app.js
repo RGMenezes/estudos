ef4252d2fb894f7f3a7487e90b0c68be60eae502
Rafael Menezes <faelgmp@gmail.com>
2023-06-06 21:42:17 -0300

    configurando sessoes

--- Curso node js/modulo2/dasafios/blog/app.js
+++ Curso node js/modulo2/dasafios/blog/app.js
@@ -7,8 +7,26 @@ const bodyParser = require('body-parser');
 const admin = require('./routes/admin')
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require("connect-flash");
 
 //Configurações
+    //Sessão
+    app.use(session({
+        secret: "podeserqualquercoisaporemdevesersegura",
+        resave: true,
+        saveUninitialized: true
+    }));
+
+    app.use(flash());
+
+    //Middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash("success_msg");
+        res.locals.error_msg = req.flash("error_msg");
+        next();
+    });
+
     //Body Parser
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
@@ -34,11 +52,6 @@ const mongoose = require('mongoose');
 
     // Public
     app.use(express.static(path.join(__dirname, "public")))
-
-    app.use((req, res, next) => {
-        console.log("OI eu sou um middleware");
-        next();
-    });
 
 //Rotas
 
