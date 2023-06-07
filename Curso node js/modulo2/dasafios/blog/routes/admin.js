0ddfc9e1c8265b4c3c609ea182188f9029cbe183
Rafael Menezes <faelgmp@gmail.com>
2023-06-06 23:22:30 -0300

    Listando categorias

--- Curso node js/modulo2/dasafios/blog/routes/admin.js
+++ Curso node js/modulo2/dasafios/blog/routes/admin.js
@@ -13,7 +13,12 @@ router.get("/posts", (req, res) => {
 });
 
 router.get('/categorias', (req, res) => {
-    res.render("admin/categorias");
+    Categorias.find().sort({date: "desc"}).then((categorias) => {
+        res.render("admin/categorias", {categorias: categorias});
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as categorias");
+        res.redirect("/admin");
+    });
 });
 
 router.get('/categorias/add', (req, res) => {
