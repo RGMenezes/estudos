57ce52f94d017de7297c2bf2f77f740a1f8b5a5f
Rafael Menezes <faelgmp@gmail.com>
2023-06-06 21:33:12 -0300

    criando um middleware

--- Curso node js/modulo2/dasafios/blog/app.js
+++ Curso node js/modulo2/dasafios/blog/app.js
@@ -35,6 +35,11 @@ const mongoose = require('mongoose');
     // Public
     app.use(express.static(path.join(__dirname, "public")))
 
+    app.use((req, res, next) => {
+        console.log("OI eu sou um middleware");
+        next();
+    });
+
 //Rotas
 
 app.get('/', (req, res) => {
