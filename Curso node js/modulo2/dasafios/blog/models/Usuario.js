9b311f152802797fe602546b36f292f481d30ae5
Rafael Menezes <faelgmp@gmail.com>
2023-06-10 11:57:30 -0300

    Criando o model de usuario

---
+++ Curso node js/modulo2/dasafios/blog/models/Usuario.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Usuario = new Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    senha: {
+        type: String,
+        require: true
+    }
+});
+
+mongoose.model("usuarios", Usuario);
