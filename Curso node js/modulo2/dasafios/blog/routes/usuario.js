57f4b47a2e753f2b40059305d93f79d7093083b5
Rafael Menezes <faelgmp@gmail.com>
2023-06-11 10:50:34 -0300

    criando o logout

--- Curso node js/modulo2/dasafios/blog/routes/usuario.js
+++ Curso node js/modulo2/dasafios/blog/routes/usuario.js
@@ -87,6 +87,17 @@ router.post("/login", (req, res, next) => {
     })(req, res, next);
 });
 
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if(err){
+            next(err);
+        }else{
+            req.flash("success_msg", "Deslogado com sucesso!");
+            res.redirect("/");
+        };
+    });
+});
+
 
 
 module.exports = router;
