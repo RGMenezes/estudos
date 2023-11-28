db583706b41c737931b8808233cb679db5fa4700
Rafael Menezes <faelgmp@gmail.com>
2023-11-28 17:57:12 -0300

    tipo string

---
+++ Curso TS/modulos/modulo02/string/string.js
@@ -0,0 +1,4 @@
+"use strict";
+// String != string.
+let assistente = "Como está o seu dia?";
+console.log(`O assistente está dizendo: ${assistente}`);
---
+++ Curso TS/modulos/modulo02/string/string.ts
@@ -0,0 +1,5 @@
+// String != string.
+
+let assistente:string = "Como está o seu dia?";
+
+console.log(`O assistente está dizendo: ${assistente}`);
