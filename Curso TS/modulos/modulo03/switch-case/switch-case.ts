3c01d33dba07282ac29d19c3d68256bf54962a24
Rafael Menezes <faelgmp@gmail.com>
2023-12-01 23:20:51 -0300

    switch-case em ts

---
+++ Curso TS/modulos/modulo03/switch-case/switch-case.ts
@@ -0,0 +1,13 @@
+//Igual ao JavaScript!
+
+
+const frase: string = 'Se vai ler isso mesmo?';
+
+switch (frase) {
+  case "Desperdicio de tempo.":
+  break
+  case "Vai lavar uma louça!":
+  break
+  default:
+  break
+};
