6fa0aa4a09983221fd2e6e2e4bb37acb0bd1397e
Rafael Menezes <faelgmp@gmail.com>
2023-12-01 21:58:28 -0300

    tipo null e undefined

---
+++ Curso TS/modulos/modulo02/null_undefined/null_undefined.ts
@@ -0,0 +1,31 @@
+/* Null
+
+  - Ausência intencional de valor. 
+  - Variável não inicializada.
+  - Aceita apenas um valor.
+  - Tipo null só atribui null.
+*/
+
+/* Undefined
+
+ - Variaveis não inicializadas.
+ - Tem apenas um valor. 
+ - Função que não retorna valor.
+*/
+
+//Null não é igual a Undefined.
+
+
+let variavelTeste = null;
+
+console.log(variavelTeste);
+console.log(typeof variavelTeste);
+
+
+let variavelTeste2;
+
+console.log(variavelTeste2);
+console.log(typeof variavelTeste2);
+
+
+export{} //Evita conflitos de escopo entre arquivos.
